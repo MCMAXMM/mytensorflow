dc5c72a02230937c629fe69a1371f4a03797d35b
MCMAXMM <32216793+MCMAXMM@users.noreply.github.com>
2018-09-07 21:17:27 +0800

    Update tf_eval.py

--- tf_eval.py
+++ tf_eval.py
@@ -2,6 +2,12 @@ import tensorflow as tf
 a=tf.constant(1.0)
 b=tf.constant(2.0)
 c=a+b
+p = tf.placeholder(tf.float32)
+t = p + 1.0
+
 with tf.Session() as sess:
 #如果你使用了eval()，你就不用使用sess去run it
   print(c.eval()
+  print(t.eval())  # This will fail, since the placeholder did not get a value.
+  print(t.eval(feed_dict={p:2.0}))  # This will succeed because we're feeding a value
+                           # to the placeholder.
