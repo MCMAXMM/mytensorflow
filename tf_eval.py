f6b448c3f024ab52f5e194aea89837f25a6291e4
MCMAXMM <32216793+MCMAXMM@users.noreply.github.com>
2018-09-07 21:24:07 +0800

    Update tf_eval.py

--- tf_eval.py
+++ tf_eval.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+#1.常数形式
 a=tf.constant(1.0)
 b=tf.constant(2.0)
 c=a+b
@@ -11,3 +12,11 @@ with tf.Session() as sess:
   print(t.eval())  # This will fail, since the placeholder did not get a value.
   print(t.eval(feed_dict={p:2.0}))  # This will succeed because we're feeding a value
                            # to the placeholder.
+#
+#
+#
+#2.变量形式
+my_variable = tf.get_variable("my_variable", [1, 2, 3])
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())#需要初始化下
+    print(my_variable.eval())
