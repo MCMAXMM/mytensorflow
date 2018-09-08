2ed67632d6648bcc3456fcc0f6eea40d45bf6327
MCMAXMM <32216793+MCMAXMM@users.noreply.github.com>
2018-09-08 19:13:07 +0800

    Update graph.py

--- graph.py
+++ graph.py
@@ -5,6 +5,7 @@ a=tf.Graph()
 #session要和它的图对应，不指定的话，session对应的就是默认图
 with a.as_default():
     b=tf.get_variable("input",initializer=np.ones((2,3)))
+    c=tf.get_default_graph()
 with tf.Session(graph=a) as sess:
     sess.run(tf.global_variables_initializer())
     print(sess.run(b))
