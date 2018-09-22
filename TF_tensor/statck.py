373ee43910bb48ab4ed4d7d785c44ee239affc1c
MCMAXMM <32216793+MCMAXMM@users.noreply.github.com>
2018-09-22 15:49:40 +0800

    Create statck.py

---
+++ TF_tensor/statck.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+tf.enable_eager_execution()
+x = tf.constant([[1, 4],[2,8]])
+y = tf.constant([[2, 5],[4,10]])
+z = tf.constant([[3, 6],[6,12]])
+a=tf.stack([x, y, z],axis=0) #[3,2,2] 注意3的位置与axis一致
+#a中axis=0，最简单直接将想x,y,z按照顺序堆叠起来，
+b=tf.stack([x, y, z], axis=1) #[2,3,2]
+#b中先将x[0],y[0],z[0]堆叠到一块，再将x[1],y[1],z[1]堆叠起来
+c=tf.stack([x,y,z],axis=2)#[2,2,3]
+#c中先将x[0][0],y[0][0],z[0][0],堆成一列，再将想x[0][1],y[0][1],z[0][1]堆成一列
+print(a)
+print(b)
+print(c)
