5522a364272ffebf6212cd1f7c9ff04094c6e398
MCMAXMM <32216793+MCMAXMM@users.noreply.github.com>
2018-09-22 09:28:41 +0800

    Create tf_image.py

---
+++ tf_operation/tf_image.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+from tensorflow import keras
+#使用keras下载文件需要制定文件的名称，保存的路径以及下载的路径
+path_to_zip=tf.keras.utils.get_file('facades.tar.gz',
+               cache_subdir=os.path.abspath('.'),
+             origin='https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/facades.tar.gz',
+               extract=True)
+# PATH=os.path.join(os.path.dirname(path_to_zip),"facades/")
+#os.path.abspath(".")返回当前路径的绝对路径
+image=tf.read_file("D:\pythonrun\open_cv\hello.jpg")#使用tf读取图片，此时读出来的数据是二进制数据
+image=tf.image.decode_jpeg(image)#相当于将二进制数据解码成jpeg格式，将二进制数据转换成图片矩阵tensor格式,显示的数据
+#有其他的如.decode_bmp() .decode_gif(),.decode_png()等等
+#是0—255之间的tensor数据
+print(image)
