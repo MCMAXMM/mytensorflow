dff5e3dce5c6da4e545f60600a0b39dd0fe8a4f3
MCMAXMM <32216793+MCMAXMM@users.noreply.github.com>
2018-12-11 16:12:03 +0800

    Create Boolean_mask.py

---
+++ TF_tensor/Boolean_mask.py
@@ -0,0 +1,10 @@
+import tensorflow as tf 
+# 1-D example
+tensor = [0, 1, 2, 3]
+mask = np.array([True, False, True, False])
+boolean_mask(tensor, mask)  # [0, 2]
+
+# 2-D example
+tensor = [[1, 2], [3, 4], [5, 6]]
+mask = np.array([True, False, True])
+boolean_mask(tensor, mask)  # [[1, 2], [5, 6]]
