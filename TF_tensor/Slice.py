364a8bd4d48634cd84860f50922a5a93d798f92c
MCMAXMM <32216793+MCMAXMM@users.noreply.github.com>
2018-12-14 10:09:57 +0800

    Create Slice.py

---
+++ TF_tensor/Slice.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+tf.enable_eager_execution()
+#tf.slice( input_,begin, size, name=None)
+#从哪里开始（begin）,取多大(size)的张量
+t = tf.constant([[[1, 1, 1], [2, 2, 2]],
+                 [[3, 3, 3], [4, 4, 4]],
+                 [[5, 5, 5], [6, 6, 6]]])
+tf.slice(t, [1, 0, 0], [1, 1, 3])  # [[[3, 3, 3]]]
+tf.slice(t, [1, 0, 0], [1, 2, 3])  # [[[3, 3, 3],
+                                   #   [4, 4, 4]]]
+tf.slice(t, [1, 0, 0], [2, 1, 3])  # [[[3, 3, 3]],
+                                   #  [[5, 5, 5]]]
