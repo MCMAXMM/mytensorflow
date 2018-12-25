bb4c1d6339a10938a03f9fec67c1cbfcea5f8f2d
MCMAXMM <32216793+MCMAXMM@users.noreply.github.com>
2018-12-25 13:43:52 +0800

    Update tf_image.py

--- tf_image.py
+++ tf_image.py
@@ -6,3 +6,9 @@ print(file.get_next())
 print(file.get_next())
 #tf.Tensor(b'D:\\download\\Downloads\\facades\\train\\389.jpg', shape=(), dtype=string)
 #tf.Tensor(b'D:\\download\\Downloads\\facades\\train\\315.jpg', shape=(), dtype=string)
+
+train_dataset = tf.data.Dataset.list_files(PATH+'train/*.jpg')
+train_dataset = train_dataset.shuffle(BUFFER_SIZE)
+train_dataset = train_dataset.map(lambda x: load_image(x, True))
+#train_dataset是一个可以迭代的东西，map将train_dataset中的每一个元素应用于一个函数
+train_dataset = train_dataset.batch(1)
