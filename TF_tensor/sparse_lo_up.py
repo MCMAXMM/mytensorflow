947c859743e0b91e5c4427daf2bd18c8a2a5a281
MCMAXMM <32216793+MCMAXMM@users.noreply.github.com>
2019-05-11 23:33:28 +0800

    Update sparse_lo_up.py

--- TF_tensor/sparse_lo_up.py
+++ TF_tensor/sparse_lo_up.py
@@ -16,7 +16,8 @@ b = tf.Variable(b, dtype=tf.float32)
 c = tf.Variable(c, dtype=tf.float32)
 
 idx = tf.SparseTensor(indices=[[0,0], [0,2], [1,0], [1, 1]], values=[1,1,2,0], dense_shape=(2,3))
-result = tf.nn.embedding_lookup_sparse(a, idx, None, combiner=None)
+result = tf.nn.embedding_lookup_sparse(a, idx, idx, combiner=None)
+#将idx当成类weight,weight也是稀疏的
 #解释一下就是就是取idx中每一行对应的值所对应的向量求平均（在没给weight的条件下
 # 如果给weight的话就是加权和）
 init = tf.global_variables_initializer()
@@ -24,3 +25,14 @@ sess = tf.Session()
 sess.run(init)
 r = sess.run(result)
 print(r)
+####输出为#####
+
+#embedding:
+[[ 0  1  2  3]
+ [ 4  5  6  7]
+ [ 8  9 10 11]
+ [12 13 14 15]]
+WARNING:tensorflow:The default value of combiner will change from "mean" to "sqrtn" after 2016/11/01.
+2019-05-11 23:30:52.694134: I tensorflow/core/platform/cpu_feature_guard.cc:141] Your CPU supports instructions that this TensorFlow binary was not compiled to use: AVX2
+[[ 4.  5.  6.  7.]#第一行的加权和 ，分别为embedding中的1,1，权重为1,1
+ [ 8.  9. 10. 11.]]#第二行的加权和，分别为embedding中2,0，权重为2,0 最后除以向量数目
